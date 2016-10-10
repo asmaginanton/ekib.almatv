b92acb51bbf1cb36e8e5e6534840aec87f3dc7d0
Anton Smagin <asmaginanton@gmail.com>
2016-10-10 21:05:38 +0600

    recavery vendor/kartik-v/bootstrap-fileinput folder

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql96.1gb.ru;dbname=gb_ekib',
+    'username' => 'gb_ekib',
+    'password' => '8f6eaeazdsgh',
+    'charset' => 'utf8',
+];
