c41cefe6dac6771cb764cb4321516ede0ee3e13c
Anton Smagin <asmaginanton@gmail.com>
2016-10-10 20:55:01 +0600

    few changes

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ekib.almatv',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
